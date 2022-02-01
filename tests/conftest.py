89867afb676ee0f71dccef1b061ca209b72dd392
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2022-01-31 23:18:43 -0500

    test: added some tests and fixed bugs (#6)
    * test: added some tests and fixed bugs
    
    * docs: add extra notes

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def sudo(accounts):
+    return accounts[-1]
+
+
+@pytest.fixture
+def token(sudo, project):
+    return sudo.deploy(project.Token)
+
+
+@pytest.fixture
+def vault(sudo, token, project):
+    return sudo.deploy(project.Vault, token)
