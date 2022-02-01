aec0e5234849741dc02eb5fb2e62751d4c0db941
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2022-01-31 23:28:45 -0500

    refactor: use EIP (#7)
    * refactor: get EIP from public repo
    
    * refactor: note that doc lives upstream

--- scripts/make_interfaces.py
+++ scripts/make_interfaces.py
@@ -1,20 +1,20 @@
 import re
 import yaml
 import json
+import requests
 from pathlib import Path
 
 import jsbeautifier
 
 
-README_FILE = Path(__file__).parent.parent / "README.md"
-
-YAML_CONTENT_PATTERN = r"```yaml([.\n]*)```"
+ERC4626_URL = "https://raw.githubusercontent.com/ethereum/EIPs/master/EIPS/eip-4626.md"
 
 
 def main():
+    yaml_string = requests.get(ERC4626_URL).content.decode("utf-8")
     yaml_sections = [
         s.split("```")[0]
-        for s in README_FILE.read_text().split("```yaml")
+        for s in yaml_string.split("```yaml")
         if "```" in s  # Skip first section
     ]
     spec = yaml.safe_load("".join(yaml_sections))
